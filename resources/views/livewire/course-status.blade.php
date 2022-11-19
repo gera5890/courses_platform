5549f006863e0996bdc47fcb6da49c721ef3674a
Vera Velasco Gerardo <geraveravela150b@gmail.com>
2022-11-18 18:46:45 -0600

    botones next-previous 1250

--- resources/views/livewire/course-status.blade.php
+++ resources/views/livewire/course-status.blade.php
@@ -2,8 +2,32 @@
     {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
     <div class="container grid grid-cols-3 gap-8">
         <div class="col-span-2">
-            {!! $current->iframe !!}
-            {{ $current->name }}
+            <div class="embed-responsive">
+                {!! $current->iframe !!}
+            </div>
+            <h1 class="text-3xl font-bold text-gray-600 mt-4">
+                {{ $current->name }}
+            </h1>
+                @if ($current->description)
+                    <div class="text-gray-600">
+                        {{ $current->description->name }}
+                    </div>
+                @endif
+            <div class="flex items-center mt-4 cursor-pointer">
+                <i class="fas fa-toggle-off text-gray-600 text-2xl"></i>
+                <p class="text-sm ml-2">Marcar esta la unidad como culminada</p>
+            </div>
+
+            <div class="card mt-2 hover:translate-y-0">
+                <div class="card-body flex text-gray-600">
+                    @if ($this->previous)
+                    <a wire:click="changeLesson({{ $this->previous }})" class="cursor-pointer hover:text-black hover:underline duration-500">Tema anterior</a>
+                    @endif
+                    @if ($this->next)
+                    <a wire:click="changeLesson({{ $this->next }})" class="ml-auto cursor-pointer hover:text-black hover:underline duration-500">Siguiente tema</a>
+                    @endif
+                </div>
+            </div>
             <p>
                 Indice: {{ $this->index }}
             </p>
@@ -40,7 +64,7 @@
                                 @foreach ($section->lessons as $lesson)
                                     <li>
                                         <a wire:click="changeLesson({{ $lesson }})" class="cursor-pointer">
-                                            {{ $lesson->id }} @if ($lesson->completed)
+                                            {{ $lesson->name }} @if ($lesson->completed)
                                                 (completado)
                                             @endif
                                         </a>

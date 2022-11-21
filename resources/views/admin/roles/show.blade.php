df6cbdcdf8b85a3e075ba0ba3e54c64cdad0460b
Vera Velasco Gerardo <geraveravela150b@gmail.com>
2022-11-21 12:48:21 -0600

    permisos y plantilla adminLTE

---
+++ resources/views/admin/roles/show.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'UPVM')
+
+@section('content_header')
+    <h1>UPVM Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Bienvenido al panel de administracion de UPVM plataforma en linea</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop

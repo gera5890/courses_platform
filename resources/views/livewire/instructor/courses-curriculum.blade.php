1e0d108f41723978c9da5a503ae6c24d7c902599
Vera Velasco Gerardo <geraveravela150b@gmail.com>
2023-01-26 18:44:53 -0600

    crud de secciones 411

--- resources/views/livewire/instructor/courses-curriculum.blade.php
+++ resources/views/livewire/instructor/courses-curriculum.blade.php
@@ -3,4 +3,21 @@
     <x-slot name="course">
         {{ $course->slug }}
     </x-slot>
+    <h1 class="text-2xl font-bold">
+        Lecciones del curso
+    </h1>
+    <hr class="mt-2 mb-6">
+
+    @foreach ($course->sections as $item)
+        <article class="card mb-6">
+            <div class="card-body bg-gray-200">
+                <header>
+                    <h1 class="cursor-pointer">
+                        <strong>Secci&oacuten: </strong>{{ $item->name }}
+                    </h1>
+                </header>
+            </div>
+        </article>
+    @endforeach
+
 </div>
